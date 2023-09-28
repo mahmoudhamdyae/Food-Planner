5efda5c61a255a4089fff4d30ec41bd8cac8c3e6
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-09-28 18:11:09 +0300

    Meal Fragment

--- app/src/main/java/com/mahmoudhamdyae/foodplanner/view/meal/MealFragment.java
+++ app/src/main/java/com/mahmoudhamdyae/foodplanner/view/meal/MealFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,8 +20,6 @@ import com.mahmoudhamdyae.foodplanner.R;
 import com.mahmoudhamdyae.foodplanner.model.Meal;
 
 public class MealFragment extends Fragment {
-
-    private ImageView imageView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,16 +36,30 @@ public class MealFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        imageView = view.findViewById(R.id.image_view);
-        ViewCompat.setTransitionName(imageView, "meal_image");
-
         Meal meal = MealFragmentArgs.fromBundle(getArguments()).getMeal();
 
+        // Image
+        ImageView imageView = view.findViewById(R.id.image_view);
+        ViewCompat.setTransitionName(imageView, "meal_image"); // Transition
         Glide.with(requireContext())
                 .load(meal.getStrMealThumb())
                 .apply(new RequestOptions()
                         .placeholder(R.drawable.loading_img)
                         .error(R.drawable.ic_broken_image))
                 .into(imageView);
+
+        // Texts
+        TextView titleTextView = view.findViewById(R.id.title);
+        TextView fromTextView = view.findViewById(R.id.from);
+        TextView instTextView = view.findViewById(R.id.inst);
+        titleTextView.setText(meal.getStrMeal());
+        fromTextView.setText(meal.getStrArea());
+        instTextView.setText(meal.getStrInstructions());
+
+        // Button
+        Button addToCartButton = view.findViewById(R.id.add_to_cart);
+        addToCartButton.setOnClickListener(v -> {
+            Toast.makeText(requireContext(), "Add To Cart Clicked", Toast.LENGTH_SHORT).show();
+        });
     }
 }
