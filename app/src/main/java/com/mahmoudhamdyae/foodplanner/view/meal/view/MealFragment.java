a5d1f2ec188a19fb606b90c5b4ddc781d6efe97b
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-09-29 15:20:58 +0300

    Room Database

--- app/src/main/java/com/mahmoudhamdyae/foodplanner/view/meal/view/MealFragment.java
+++ app/src/main/java/com/mahmoudhamdyae/foodplanner/view/meal/view/MealFragment.java
@@ -1,7 +1,6 @@
 package com.mahmoudhamdyae.foodplanner.view.meal.view;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
